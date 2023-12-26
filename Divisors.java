f5b1d4a1339ad1be280d43338b0a4705307ae6d0
Daniel <daniel.rachamim@post.runi.ac.il>
2023-12-26 17:30:45 +0200

    Updated

--- Divisors.java
+++ Divisors.java
@@ -1,8 +1,11 @@
-/**
- *  Gets a command-line argument (int), and prints all the divisors of the given number.
- */
 public class Divisors {
 	public static void main (String[] args) {
-		//// Put your code here
+		int num = Integer.parseInt(args[0]);
+		//For statement which prints the divisors of the given command line argument using the modulo operator %
+        for (int i = 1; i <= num; i++) {
+            if (num % i == 0) {
+                System.out.println(i);
+            }
+        }
 	}
 }
--- Reverse.java
+++ Reverse.java
@@ -1,9 +1,18 @@
-/**
- * Prints a given string, backward. Then prints the middle character in the string.
- * The program expects to get one command-line argument: A string.
- */
 public class Reverse {
 	public static void main (String[] args){
-		//// Put your code here
+		String input = args[0];
+		int length = input.length();
+		int mid = length/2;
+		//Print the reversed string
+		for (int i = length-1; i >= 0 ;i--) {
+			System.out.print(input.charAt(i));
+		} 
+		System.out.println();
+		//Print the middle character
+		if (length % 2 == 0) {
+			System.out.println("The middle character is " + input.charAt(mid-1));
+		}
+		else {System.out.println("The middle character is " + input.charAt(mid));}
 	}
+
 }
