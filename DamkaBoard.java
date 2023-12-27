86baecd306eb03e62f336799f2c29354915de8d9
Daniel <daniel.rachamim@post.runi.ac.il>
2023-12-27 21:54:48 +0200

    Updated

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,19 +1,24 @@
 public class DamkaBoard {
     public static void main(String[] args) {
-        // Parse the command-line argument as an integer
-        int n = Integer.parseInt(args[0]);
+        int input = Integer.parseInt(args[0]);
+        int index_line = 1;
+        System.out.println();
 
-        // Print the damka board
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                // Print '*' for even positions, and ' ' for odd positions
-                if ((i + j) % 2 == 0) {
-                    System.out.print("* ");
+        while(index_line <= input) {
+            int x = 1;
+            // Printing the damka board using a while loop
+            while (x <= input) {
+                if ((index_line % 2 == 0)) {
+                    //Starting the row with a space if the line is even
+                    System.out.print(" *"); 
                 } else {
-                    System.out.print("  ");
+                    System.out.print("* ");
                 }
-            }
-            System.out.println();  // Move to the next line for the next row
+                x = x+1;   
+            }  
+            System.out.println();
+            index_line = index_line + 1;    
         }
     }
 }
+        
